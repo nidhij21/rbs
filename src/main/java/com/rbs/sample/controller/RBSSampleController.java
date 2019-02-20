8f645251c439f15ebedf6cfd2771249a1e2b7043
nidhij21 <46185348+nidhij21@users.noreply.github.com>
2019-02-20 13:05:27 +0530

    Update RBSSampleController.java

--- src/main/java/com/rbs/sample/controller/RBSSampleController.java
+++ src/main/java/com/rbs/sample/controller/RBSSampleController.java
@@ -1,36 +1,23 @@
 package com.rbs.sample.controller;
 
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.Session;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jms.JmsException;
-import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.core.MessageCreator;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class RBSSampleController {
-	@Autowired
-	JmsTemplate jmsTemplate;
 	
-	@RequestMapping(value = "/test", method = RequestMethod.GET)
-	public String generateBrokerDocument() {
-		try {
-			jmsTemplate.send("BrokerGenerationQueue", new MessageCreator() {
-			      @Override
-			      public Message createMessage(Session session) throws JMSException {
-			          return session.createTextMessage("Test");
-			      }
-			  });
-		} catch (JmsException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return "Test";
+	@Autowired
+	RestTemplate restTemplate;
+	
+	@RequestMapping(value = "/testRest", method = RequestMethod.GET)
+	public String testRest() {
+		ResponseEntity<String> response = restTemplate.getForEntity("http://bgc.india.rsystems.com:8085/rbs/test1", String.class);
+		System.out.println(response.getBody());
+		return response.getBody();
 	}
 
 }
